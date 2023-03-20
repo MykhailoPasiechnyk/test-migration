2add1cf17f31630c0467d7ef29e3ab58e23961ac
MykhailoPasiechnyk <pasiechnyk.ms@gmail.com>
2023-03-20 12:12:27 +0100

    add kubernetes service account in main.tf

--- 03-terraform/main.tf
+++ 03-terraform/main.tf
@@ -2,3 +2,20 @@ provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "docker-desktop"
 }
+
+resource "kubernetes_namespace" "python-job" {
+  metadata {
+    name = "python-job"
+
+    labels = {
+      type = "namespace"
+      env  = "test"
+    }
+  }
+}
+
+resource "kubernetes_service_account" "python-sa" {
+  metadata {
+    name = "python-service-account"
+  }
+}
