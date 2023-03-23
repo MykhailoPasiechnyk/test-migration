bb7c462aca265208ad91b141758cf08274eadd9a
MykhailoPasiechnyk <pasiechnyk.ms@gmail.com>
2023-03-23 10:39:35 +0100

    add output pv_name

--- 03-terraform/outputs.tf
+++ 03-terraform/outputs.tf
@@ -1,3 +1,15 @@
 output "namespace_name" {
   value = kubernetes_namespace.namespace.metadata.0.name
 }
+
+output "service_account_name" {
+  value = kubernetes_service_account.service_account.metadata.0.name
+}
+
+output "cluster_role_name" {
+  value = kubernetes_cluster_role.cluster_role.metadata.0.name
+}
+
+output "pv_name" {
+  value = kubernetes_persistent_volume.pv.metadata.0.name
+}
